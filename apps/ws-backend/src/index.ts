5fce7fec3fa311d2c30269f77a7c0b693f245877
arjunkumar811 <kumararjun26401@gmail.com>
2025-01-29 11:12:27 +0530

    \ws-backend\

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,12 +1,52 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer } from 'ws';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import { JWT_SECRET } from '@repo/backend-comman/config';
 
-const wss = new WebSocketServer({ port: 8000 });
+const wss = new WebSocketServer({ port: 8080 });
 
-wss.on('connection', function connection(ws) {
-    ws.on('message', function message(data){
-        ws.send('pong')
+wss.on('connection', (ws, request) => {
+    const url = request.url;
+
+    if (!url) {
+        return;
+    }
+
+    const queryParams = new URLSearchParams(url.split('?')[1]);
+    const token = queryParams.get('token');
+
+    if (!token) {
+        ws.send(JSON.stringify({
+            message: "Unauthorized"
+        }));
+        ws.close();
+        return;
+    }
+
+    let decoded;
+    try {
+        decoded = jwt.verify(token, JWT_SECRET);
+    } catch (err) {
+        ws.send(JSON.stringify({
+            message: "Invalid token"
+        }));
+        ws.close();
+        return;
+    }
+
+    if (typeof decoded === "string") {
+        return;
+    }
+
+    if (!decoded || !(decoded as JwtPayload).username) {
+        ws.close();
+        return;
+    }
+
+    ws.on('message', (message) => {
+        console.log(`Received message: ${message}`);
+    });
+
+    ws.on('close', () => {
+        console.log('Client disconnected');
     });
 });
-
-
-
